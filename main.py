cb44092f3d0da888af285e043c7b18e9eff5642c
Egyed József <jegyed50@gmail.com>
2023-05-11 09:06:47 +0200

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+# https://github.com/jegyed50/jegyed50-bbc-microbit-study365-blocks-03-04-a
 # JEGYED50-BBC-MICROBIT-STUDY365-BLOCKS-03-04-A
 # 30 perces analóg óra, fél másoperces időtartamok, csak LED és pause
+
 def on_button_pressed_a():
-    basic.show_string("S")
-    basic.pause(200)
     basic.show_leds("""
         . . # . .
                 . . # . .
@@ -47,12 +47,10 @@ def on_button_pressed_a():
         . . . . .
                 . . . . .
                 . . # . .
-                . . # # .
-                . . # # .
+                . . # . .
+                . . # . .
     """)
     basic.pause(500)
-    basic.clear_screen()
-    basic.show_string("E")
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_forever():
--- main.ts
+++ main.ts
@@ -1,55 +1,56 @@
-// JEGYED50-BBC-MICROBIT-STUDY365-BLOCKS-03-04-A
-// 30 perces analóg óra, fél másoperces időtartamok, csak LED és pause
-input.onButtonPressed(Button.A, function () {
+//  https://github.com/jegyed50/jegyed50-bbc-microbit-study365-blocks-03-04-a
+//  JEGYED50-BBC-MICROBIT-STUDY365-BLOCKS-03-04-A
+//  30 perces analóg óra, fél másoperces időtartamok, csak LED és pause
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     basic.showLeds(`
         . . # . .
-        . . # . .
-        . . # . .
-        . . . . .
-        . . . . .
-        `)
+                . . # . .
+                . . # . .
+                . . . . .
+                . . . . .
+    `)
     basic.pause(500)
     basic.showLeds(`
         . . . # .
-        . . . # .
-        . . # . .
-        . . . . .
-        . . . . .
-        `)
+                . . . # .
+                . . # . .
+                . . . . .
+                . . . . .
+    `)
     basic.pause(500)
     basic.showLeds(`
         . . . . .
-        . . . # #
-        . . # . .
-        . . . . .
-        . . . . .
-        `)
+                . . . # #
+                . . # . .
+                . . . . .
+                . . . . .
+    `)
     basic.pause(500)
     basic.showLeds(`
         . . . . .
-        . . . . .
-        . . # # #
-        . . . . .
-        . . . . .
-        `)
+                . . . . .
+                . . # # #
+                . . . . .
+                . . . . .
+    `)
     basic.pause(500)
     basic.showLeds(`
         . . . . .
-        . . . . .
-        . . # . .
-        . . . # #
-        . . . . .
-        `)
+                . . . . .
+                . . # . .
+                . . . # #
+                . . . . .
+    `)
     basic.pause(500)
     basic.showLeds(`
         . . . . .
-        . . . . .
-        . . # . .
-        . . # . .
-        . . # . .
-        `)
+                . . . . .
+                . . # . .
+                . . # . .
+                . . # . .
+    `)
     basic.pause(500)
 })
-basic.forever(function () {
-	
+basic.forever(function on_forever() {
+    
 })
